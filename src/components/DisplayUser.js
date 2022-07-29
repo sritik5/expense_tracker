630d091b0951a3e2300149145b17540759e642eb
sritik5 <ritik.shah@lendenclub.com>
2022-07-29 16:30:49 +0530

    Users on home page

--- src/components/DisplayUser.js
+++ src/components/DisplayUser.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from '../css/DisplayUser.module.css'
 
 export default function DisplayUser({user}) {
 
     return (
         <div>
-            {user.uname}
+            {
+                <button className={styles.userButton} onClick={()=>console.log(user)}>{user.uname}</button>
+            }
         </div>
   )
 }
