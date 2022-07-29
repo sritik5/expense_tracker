5296913242ce31be52fbe11ca47ffbbc384d8fcb
sritik5 <ritik.shah@lendenclub.com>
2022-07-29 15:53:46 +0530

    Users displayed on the home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import styles from './App.module.css';
 import Register from './components/Register';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
 
 function App() {
+  const [auth,setAuth] = useState(false);
+  
+  useEffect(() => {
+    localStorage.getItem('Username') && setAuth(true) 
+  },[]);
+
   return (
     <div className={styles.App}>
       <h1>EXPENSE TRACKER APP</h1>
-      <div className="App"><Register/></div>
+      <Routes>
+        <Route path='/' element={ auth ? <HomePage/> : <Register/>}/>
+        <Route path='/register' index element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
 
     </div>
   );
