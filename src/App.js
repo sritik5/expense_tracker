9130367928d889f4e92c1bc31b1f824726a07b19
srujamipalkar <palkarsrujami@gmail.com>
2022-07-29 15:59:40 +0530

    Login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>EXPENSE TRACKER APP</h1>
-      <div className="App"><Register/></div>
+      <div className="App">
+        {/* <Register/> */}
+      <Login/>
+      </div>
 
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,55 @@
-import React from 'react'
-import styles from '../css/Login.module.css'
+import React, {useState} from "react";
+import styles from "../css/Login.module.css";
+import bgImg from '../components/registration_photo.png'
+import { useForm } from 'react-hook-form';
+
 
 export default function Login() {
+
+  const [user , setUser] = React.useState([]);
+  const [loginUser,setLoginUser] = React.useState({});
+
+  useEffect(()=>{
+    fetch(`http://localhost:3000/users`)
+    .then (res=>res.json())
+    .then (res=>setUser(res))
+}, []);
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm()
+  const onSubmit = data => console.log(data);
   return (
-    <div>Login</div>
-  )
+    <section>
+      <div className={styles.register}>
+        <div className={styles.col-1}>
+          <h2>Login</h2>
+          {/* <span>register and enjoy the service</span> */}
+
+          <form id={styles.form} className={styles.flex}
+            
+           
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <input
+              type="text"
+              {...register("username")}
+              placeholder="username"
+            />
+
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="password"
+            />
+
+            <button className={styles.btn}> Login</button>
+          </form>
+        </div>
+       <div className={styles.col-2}>
+       <img src={bgImg} alt="" />
+       </div>
+          
+        
+      </div>
+    </section>
+  );
 }
