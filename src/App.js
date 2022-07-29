0516be518d071c60daae1a263cf74bd33881c117
dipalipatekar <dipali13051999@gmail.com>
2022-07-29 17:15:05 +0530

    homepage-desgin1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import styles from './App.module.css';
+import HomePage from './components/HomePage';
+import {Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <h1>EXPENSE TRACKER APP</h1>
+  
+      <HomePage/>
+      
 
     </div>
   );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,58 @@
 import React from 'react'
 import styles from '../css/HomePage.module.css'
+import {Link} from 'react-router-dom'
+
 
 export default function HomePage() {
+  // const [isLogin, setIsLogin] = useState(false);
+
+  // let navigate = useNavigate(); 
+  // const logoutfunc = (() => {
+  //   localStorage.clear()
+  //   navigate('/register')
+  //   window.location.reload(false);
+
+  // })
   return (
-    <div>HomePage</div>
+    <div>
+      
+        
+        <div className={styles.line}>
+        <div className={styles.heading}><Link className={styles.navText} to = "/">Expense Tracker</Link></div>
+          <div className={styles.navele}><Link className={styles.navText} to="/">Home</Link>
+          <Link className={styles.navText} to="/logout" >Logout</Link>
+          <div className={styles.ProfilePic}>
+            <img src='https://images.unsplash.com/photo-1585670210693-e7fdd16b142e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'></img>
+            </div>
+            </div>
+            </div>
+          <div className={styles.sections}>
+
+            <div className={styles.left}>
+            <span className={styles.imageleft}><img src='https://images.unsplash.com/photo-1585670210693-e7fdd16b142e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'></img></span>
+              <div className={styles.lefthead}><h1>Welcome,<span>&nbsp;username</span></h1></div>
+             
+              
+              <div className={styles.list}>
+                <div className={styles.leftname}>
+                <p>friend 1</p>
+                </div>
+
+                <div className={styles.rightamt}>
+                <p>$500</p>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.right}>
+              <h1>friends name</h1>
+            </div>
+
+            </div>
+      </div>
+     
+
+   
+
   )
 }
