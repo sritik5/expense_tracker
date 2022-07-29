a7897f053c701c12d9d61ca79d499529ef5dffa2
sampradajawalkar <sampradajawalkar@gmail.com>
2022-07-29 14:49:00 +0530

    Registration form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>EXPENSE TRACKER APP</h1>
+      <div className="App"><Register/></div>
 
     </div>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,36 @@
 import React from 'react'
+// import bgImg from '../assets/img1.jpg';
+import { useForm } from 'react-hook-form';
 import styles from '../css/Register.module.css'
+export default function Form() {
 
-export default function Register() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm()
+    const onSubmit = data => console.log(data);
+
+    // console.log(watch('username'));
+    
   return (
-    <div>Register</div>
+    <section>
+        <div className={styles.register}>
+            <div className={styles.col-1}>
+                <h2>Sign In</h2>
+                <span>register and enjoy the service</span>
+
+                <form id={styles.form} className={styles.flex} onSubmit={handleSubmit(onSubmit)}>
+                    <input type="text" {...register("username")} placeholder='username' />
+                    <input type="text" {...register("password")} placeholder='password' />
+                    <input type="text" {...register("confirmpwd")} placeholder='confirm password' />
+                    <input type="text" {...register("mobile", { required : true, maxLength: 10 })} placeholder='mobile number' />
+                    {errors.mobile?.type === "required" && "Mobile Number is required"}
+                    {errors.mobile?.type === "maxLength" && "Max Length Exceed"}
+                    <button className={styles.btn}>Sign In</button>
+                </form>
+
+            </div>
+            <div className={styles.col-2}>
+                {/* <img src={bgImg} alt="" /> */}
+            </div>
+        </div>
+    </section>
   )
 }
