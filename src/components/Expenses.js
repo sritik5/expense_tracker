e016db4213d7a2be44e87534ae10d5ccbb529bc7
srujamipalkar <srujami.palkar@lendenclub.com>
2022-08-06 17:34:06 +0530

    Add Expense Form

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import ExpenseForm from './ExpenseForm';
 
 export default function Expenses({user1, user2}) {
 
     const [transactions, setTransactions] = useState();
     const [userTransactions, setUserTransactions] = useState();
+    const [displayForm, setDisplayForm] = useState(false);
 
     useEffect(() => {
         fetch('http://localhost:3000/transaction')
@@ -16,13 +18,19 @@ export default function Expenses({user1, user2}) {
 
     useEffect(() => {
         if(transactions)
+        {
+            console.log(user1)
+            console.log(user2)
+
             transactions.filter(t => {
-                if(t.uname1 === user1 || t.uname2 === user2)
+                if(t.uname1 === user1 && t.uname2 === user2)
                     setUserTransactions(t.data)
-                else if(t.uname1 === user2 || t.uname2 === user1)
+                else if(t.uname1 === user2 && t.uname2 === user1)
                     setUserTransactions(t.data)
             } )
-    }, [transactions])
+        }
+        
+    }, [transactions, user1, user2])
     
     // if(userTransactions)
     // {
@@ -31,12 +39,21 @@ export default function Expenses({user1, user2}) {
     // }
 
     return (
+
         <div>
             <h1>{user2}</h1>
             {
                 userTransactions &&
                 userTransactions.map((t,index) => <h3 key={index}>{t.desc} {t.money} {t.paid}</h3>)
             }
+            <div>
+                <button onClick={()=>setDisplayForm(true)}>Add Expense</button>
+                {
+                    displayForm &&
+                    <ExpenseForm user1={user1} user2={user2} />
+                }
+
+            </div>
         </div>
   )
 }
